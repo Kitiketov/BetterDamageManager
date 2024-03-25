4e53683cf1a1c8c268e3a3208997c200df99baac
Kitiketov <roskets801@gmail.com>
2024-03-25 12:55:40 +0500

    upd

--- handler.py
+++ handler.py
@@ -1,10 +1,9 @@
-from PyQt5 import QtWidgets, QtCore
+import os
+from datetime import datetime, timezone
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import QFileSystemWatcher, QIODevice
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-from PyQt5.QtCore import QThread,QFileSystemWatcher, QIODevice
-from PIL import Image,ImageGrab
-
-import os,time
-
 
 
 class Handler():
@@ -24,7 +23,8 @@ class Handler():
         for port in ports:
             self.portList.append(port.portName())
         self.ui.comBox.addItems(self.portList)
-    
+        self.ui.folderPath.setText(self.path_to_txt)
+
     def show_message(self, widget, message=''):
         if message:
             widget.setText(message)
@@ -33,7 +33,7 @@ class Handler():
         self.timer.start(2000)
 
     def get_path_to_file(self):
-        return QtWidgets.QFileDialog.getOpenFileName(self.MyWindow, 'выбрать путь к файлу', '', 'Текстовый документ (*.txt)')[0]
+        return QtWidgets.QFileDialog.getOpenFileName(self.MyWindow, 'выбрать путь к файлу', '','Текстовый документ (*.txt)')[0]
 
     def edit_file_folder(self):
         _path = self.get_path_to_file()
@@ -42,20 +42,29 @@ class Handler():
             self.show_message(self.ui.errorMessage, 'No file selected')
             return 'Error'
         self.path_to_txt = _path
+
         self.ui.folderPath.setText(self.path_to_txt)
 
-    def test_write(self):
-        with open(self.path_to_txt,"w+") as f:
-            f.write(f"vibro/1/{int(time.time())}\n")
+    def test_write(self, mode):
+        with open(self.path_to_txt, "w+") as f:
+            f.write(f"{mode}/1/{int(datetime.now(timezone.utc).timestamp())}\n")
 
-    def file_changed(self,path):
-        print('File Changed: %s' % path)
+    def file_changed(self, path):
         with open(self.path_to_txt) as f:
-            for line in f:
-                pass
-        last_line = line.split("/")
-        text = last_line[0]
-        self.show_message(self.ui.status,text)
+            line = f.readline()
+
+        mode, dmg, time = line.split("/")
+        ts = int(time)
+
+        if mode == "shock":
+            self.sendSerial('s', int(dmg))
+        elif mode == "vibro":
+            self.sendSerial('v', int(dmg))
+
+        text = datetime.fromtimestamp(ts).strftime('%H:%M:%S ') + mode + "\n"
+        self.ui.console.setPlainText(self.ui.console.toPlainText() + text)
+        self.ui.console.moveCursor(QtGui.QTextCursor.End)
+        self.ui.console.ensureCursorVisible()
 
     def start_tracking(self):
         if not os.path.isfile(self.path_to_txt):
@@ -68,31 +77,28 @@ class Handler():
 
         self.swithButton(self.ui.startButton, "Stop tracking", self.stop_tracking)
 
-    
     def stop_tracking(self):
         self.watcher.removePath(self.path_to_txt)
         self.ui.editFolderPath.setEnabled(True)
         self.ui.label.setEnabled(False)
 
-        self.swithButton(self.ui.startButton, "Start tracking",self.start_tracking)
+        self.swithButton(self.ui.startButton, "Start tracking", self.start_tracking)
 
     def openPort(self):
         self.serial.setPortName(self.ui.comBox.currentText())
         self.serial.open(QIODevice.ReadWrite)
         self.ui.comBox.setEnabled(False)
-        self.swithButton(self.ui.openPortButton, "Close port",self.closePort)
+        self.swithButton(self.ui.openPortButton, "Close port", self.closePort)
 
     def closePort(self):
         self.serial.close()
         self.ui.comBox.setEnabled(True)
-        self.swithButton(self.ui.openPortButton, "Open port",self.openPort)
+        self.swithButton(self.ui.openPortButton, "Open port", self.openPort)
 
-
-    def swithButton(self,widget,title,func):
+    def swithButton(self, widget, title, func):
         widget.disconnect()
         widget.setText(title)
         widget.clicked.connect(lambda: func())
 
-    def sendSerial(self):
-        self.serial.write(b'1')
-        
+    def sendSerial(self, mode, value):
+        self.serial.write(f"{mode}{value}".encode())
